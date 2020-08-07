c4389b015204daa12de102c7ae0b1da331bfc0fa
Derek Smart <smart@automattic.com>
2020-08-07 09:25:14 -0400

    triager initial

---
+++ .github/actions/auto-assign/src/index.js
@@ -0,0 +1,51 @@
+/**
+ * External dependencies
+ */
+const { setFailed, getInput } = require( '@actions/core' );
+const { context, getOctokit } = require( '@actions/github' );
+
+/**
+ * Internal dependencies
+ */
+const assignIssues = require( './tasks/assign-issues' );
+const debug = require( './debug' );
+
+const automations = [
+	{
+		event: 'issues',
+		action: [ 'opened', 'synchronize', 'edited' ],
+		task: assignIssues,
+	},
+];
+
+( async function main() {
+	const token = getInput( 'github_token' );
+	if ( ! token ) {
+		setFailed( 'main: Input `github_token` is required' );
+		return;
+	}
+
+	const octokit = new getOctokit( token );
+
+	// Get info about the event.
+	const eventPayload = context.payload;
+	const eventAction = eventPayload.action;
+
+	debug( `main: Received event = '${ context.eventName }', action = '${ eventPayload.action }'` );
+
+	for ( const { event, action, task } of automations ) {
+		if (
+			event === context.eventName &&
+			( action === undefined || action.includes( eventAction ) )
+		) {
+			try {
+				debug( `main: Starting task ${ task.name }` );
+				await task( eventPayload, octokit );
+			} catch ( error ) {
+				setFailed( `main: Task ${ task.name } failed with error: ${ error }` );
+			}
+		}
+	}
+
+	debug( 'main: All done!' );
+} )();
