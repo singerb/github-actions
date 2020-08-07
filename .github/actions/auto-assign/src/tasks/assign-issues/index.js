85eb9d3121707eae485d9848544e08fc68f0391b
Derek Smart <smart@automattic.com>
2020-08-07 11:10:33 -0400

    just the name

--- .github/actions/auto-assign/src/tasks/assign-issues/index.js
+++ .github/actions/auto-assign/src/tasks/assign-issues/index.js
@@ -15,7 +15,7 @@ const debug = require( '../../debug' );
  * @param {GitHub}                    octokit Initialized Octokit REST client.
  */
 async function assignIssues( payload, octokit ) {
-	jq.run( '.issue.labels[]', JSON.stringify( payload ), { input: 'string' } )
+	jq.run( '.issue.labels[].name', JSON.stringify( payload ), { input: 'string' } )
 		.then( ( output ) => {
 			debug( output )
 		} )
