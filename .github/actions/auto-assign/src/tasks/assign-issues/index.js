1fde8dcb40f7cdf6277a934b3137628dda4e4315
Derek Smart <smart@automattic.com>
2020-08-07 10:36:13 -0400

    catch it

--- .github/actions/auto-assign/src/tasks/assign-issues/index.js
+++ .github/actions/auto-assign/src/tasks/assign-issues/index.js
@@ -15,9 +15,13 @@ const debug = require( '../../debug' );
  * @param {GitHub}                    octokit Initialized Octokit REST client.
  */
 async function assignIssues( payload, octokit ) {
-	jq.run( '.labels', { payload }, {} )
-		.then( debug )
-	debug( JSON.stringify( payload ) );
+	jq.run( '.labels', JSON.stringify( payload ), {} )
+		.then( ( output ) => {
+			debug( output )
+		} )
+		.catch( ( err ) => {
+			debug( err )
+		} )
 	// const regex = /(?:close|closes|closed|fix|fixes|fixed|resolve|resolves|resolved):? +(?:\#{1}|https?:\/\/github\.com\/automattic\/jetpack\/issues\/)(\d+)/gi;
 	//
 	// let match;
