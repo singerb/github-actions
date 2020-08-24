dcf2ee1b836706b73810b1394a640c9b2ecee99b
Benedict Singer <singerb@mac.com>
2020-08-24 11:51:29 -0400

    debug log

--- .github/actions/auto-assign/src/tasks/assign-issues/index.js
+++ .github/actions/auto-assign/src/tasks/assign-issues/index.js
@@ -22,6 +22,7 @@ async function assignIssues( payload, octokit ) {
 	} )
 
 	debug( 'Found labels: ' + labels.toString() );
+	debug( 'Current assignees: ' + JSON.stringify( payload.assignees ) );
 
 	debug( 'Looking for assignee that matches labels...' );
 
